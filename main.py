e73ea6852afc6ea517c2448ce3f42c18e015a2b9
kuralamuthan300 <kuralamuthan300@gmail.com>
2024-05-24 10:21:12 +0530

    data ingestion tested with main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import software_defect_prediction.config
-from software_defect_prediction.components import data_ingestion
+from software_defect_prediction.config.configuration import ConfigurationManager
+from software_defect_prediction.components.data_ingestion import Data_Ingestion
+from software_defect_prediction import logger
 
-
+configuration_obj = ConfigurationManager()
+data_ingestion_conf =  configuration_obj.get_data_ingestion_config()
+step_data_ing = Data_Ingestion(data_ingestion_conf)
+step_data_ing.download_file()
+step_data_ing.unzip_file()
+logger.success("Data ingestion completed successfully")
--- src/software_defect_prediction/components/data_ingestion.py
+++ src/software_defect_prediction/components/data_ingestion.py
@@ -4,14 +4,15 @@ from software_defect_prediction import logger
 import urllib.request
 from pathlib import Path
 import zipfile
+import os
 
  
-class data_ingestion():
+class Data_Ingestion():
     def __init__(self,data_ingestion_config : DataIngestionConfig) -> None:
-        config = data_ingestion_config
+        self.config = data_ingestion_config
     
     def download_file(self) -> None:
-        save_path = Path(config.root_dir,config.source_URL_file_name)
+        save_path = Path(self.config.root_dir,self.config.source_URL_file_name)
         try:
             # Open a connection to the URL
             with urllib.request.urlopen(self.config.source_URL) as response:
@@ -26,7 +27,13 @@ class data_ingestion():
         except Exception as e:
             logger.error(f"Failed to download file from {self.config.source_URL}: {e}")
             
-    def unzip_file(zip_file_path, extract_dir):
+    def unzip_file(self) -> None:
+        
+        zip_file_path = Path(self.config.root_dir,self.config.source_URL_file_name) 
+        extract_dir = self.config.unzip_dir
+        from_rename = self.config.source_URL_unzip_file_name
+        to_rename = self.config.local_data_file
+        
         try:
             # Create the extraction directory if it doesn't exist
             os.makedirs(extract_dir, exist_ok=True)
@@ -35,6 +42,7 @@ class data_ingestion():
             with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
                 # Extract all contents of the zip file to the extraction directory
                 zip_ref.extractall(extract_dir)
+                os.replace(Path(extract_dir,from_rename),Path(extract_dir,to_rename))
 
             logger.info(f"Successfully extracted {zip_file_path} to {extract_dir}")
         except Exception as e:
